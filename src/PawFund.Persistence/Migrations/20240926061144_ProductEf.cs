ffb2267a82d9c78893cd137de362028eb8b36b04
Bentanick <kennyjamqw@gmail.com>
2024-09-26 13:12:19 +0700

    add-configuration

---
+++ src/PawFund.Persistence/Migrations/20240926061144_ProductEf.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PawFund.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProductEf : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Products",
+                table: "Products");
+
+            migrationBuilder.RenameTable(
+                name: "Products",
+                newName: "Products_VietVy");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Products_VietVy",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Products_VietVy",
+                table: "Products_VietVy",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Products_VietVy",
+                table: "Products_VietVy");
+
+            migrationBuilder.RenameTable(
+                name: "Products_VietVy",
+                newName: "Products");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Products",
+                table: "Products",
+                column: "Id");
+        }
+    }
+}
