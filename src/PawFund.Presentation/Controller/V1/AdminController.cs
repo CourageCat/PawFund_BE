3458bfd1c70492491a6c9fb9bbcf99fac7d7d7cb
Lâm Phát <tphatt22@gmail.com>
2024-09-27 12:20:04 +0700

    create adminController

---
+++ src/PawFund.Presentation/Controller/V1/AdminController.cs
@@ -0,0 +1,30 @@
+﻿
+
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PawFund.Presentation.Abstractions;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace PawFund.Presentation.Controller.V1
+{
+    public class AdminController : ApiController
+    {
+        public AdminController(ISender sender) : base(sender)
+        {
+        }
+
+
+        //[HttpPost(Name = "CreateProducts")]
+        //[ProducesResponseType(StatusCodes.Status200OK)]
+        //[ProducesResponseType(StatusCodes.Status404NotFound)]
+        //public async Task<IActionResult> Products([FromBody] Command.CreateProductCommand CreateProduct)
+        //{
+        //    var result = await Sender.Send(CreateProduct);
+        //    if (result.IsFailure)
+        //        return HandlerFailure(result);
+
+        //    return Ok(result);
+        //}
+    }
+}
