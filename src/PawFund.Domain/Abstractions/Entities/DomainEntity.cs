b7788601bfe7217b45d87c8b70d569fdb260a5ef
Bentanick <kennyjamqw@gmail.com>
2024-09-28 12:46:43 +0700

    commit

--- src/PawFund.Domain/Abstractions/Entities/DomainEntity.cs
+++ src/PawFund.Domain/Abstractions/Entities/DomainEntity.cs
@@ -3,7 +3,9 @@
 public class DomainEntity<TKey>
 {
     public virtual TKey Id { get; set; }
-
+    public DateTime? CreatedDate { get; set; }
+    public DateTime? ModifiedDate { get; set; }
+    public bool? IsDeleted { get; set; }
     /// <summary>
     /// True if domain entity has an identity
     /// </summary>
---
+++ src/PawFund.Domain/Entities/Account.cs
@@ -0,0 +1,13 @@
+﻿using PawFund.Domain.Abstractions.Entities;
+
+namespace PawFund.Domain.Entities;
+
+public class Account : DomainEntity<Guid>
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public bool Status { get; set; }
+    public int RoleId { get; set; }
+}
