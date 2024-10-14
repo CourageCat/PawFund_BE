61f57d662dc8a44bb745bdeec634329ef9434feb
Bentanick <kennyjamqw@gmail.com>
2024-10-14 17:50:44 +0700

    Commit

--- src/PawFund.Application/Mapper/ProductProfile.cs
+++ src/PawFund.Application/Mapper/ProductProfile.cs
@@ -9,10 +9,7 @@ public class ProductProfile : Profile
 {
     public ProductProfile()
     {
-        CreateMap<PagedResult<Product>, PagedResult<ProductResponse>>()
-            .ForMember(dest => dest.Items, opt => opt.MapFrom(src => src.Items));
-
-        CreateMap<PagedResult<ProductResponse>, PagedResult<Product>>()
-            .ForMember(dest => dest.Items, opt => opt.MapFrom(src => src.Items));
+        CreateMap<Product, ProductResponse>().ReverseMap();
+        CreateMap<PagedResult<Product>, PagedResult<ProductResponse>>().ReverseMap();
     }
 }
