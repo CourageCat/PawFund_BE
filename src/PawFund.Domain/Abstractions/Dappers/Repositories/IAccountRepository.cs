47f505b62f4a657e71455df498f5dcbc2c74adf4
Bentanick <kennyjamqw@gmail.com>
2024-10-17 02:24:25 +0700

    commit payos

--- src/PawFund.Domain/Abstractions/Dappers/Repositories/IAccountRepository.cs
+++ src/PawFund.Domain/Abstractions/Dappers/Repositories/IAccountRepository.cs
@@ -1,4 +1,6 @@
 ﻿
+using PawFund.Contract.Abstractions.Shared;
+using PawFund.Contract.Services.Products;
 using PawFund.Domain.Entities;
 
 namespace PawFund.Domain.Abstractions.Dappers.Repositories;
@@ -7,4 +9,5 @@ public interface IAccountRepository : IGenericRepository<Domain.Entities.Account
 {
     Task<bool> EmailExistSystem(string email);
     Task<Account> GetByEmailAsync(string email);
+    //Task<PagedResult<Account>> GetPagedAsync(int pageIndex, int pageSize, Filter.ProductFilter filterParams, string[] selectedColumns)
 }
