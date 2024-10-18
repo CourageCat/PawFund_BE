94cb8becb1ca0433107c4f49d5fc3432b892c215
Bentanick <kennyjamqw@gmail.com>
2024-10-19 04:41:08 +0700

    commit

--- src/PawFund.Application/UseCases/V1/Commands/Cat/CreateCatCommandHandler.cs
+++ src/PawFund.Application/UseCases/V1/Commands/Cat/CreateCatCommandHandler.cs
@@ -4,6 +4,7 @@ using PawFund.Contract.Shared;
 using PawFund.Domain.Abstractions.Repositories;
 using PawFund.Domain.Abstractions;
 using PawFund.Domain.Exceptions;
+using PawFund.Contract.Abstractions.Services;
 
 namespace PawFund.Application.UseCases.V1.Commands.Cat
 {
@@ -12,21 +13,24 @@ namespace PawFund.Application.UseCases.V1.Commands.Cat
         private readonly IRepositoryBase<Domain.Entities.Cat, Guid> _catRepository;
         private readonly IRepositoryBase<Domain.Entities.Branch, Guid> _branchRepository;
         private readonly IEFUnitOfWork _efUnitOfWork;
+        private readonly IMediaService _mediaService;
 
-        public CreateCatCommandHandler(IRepositoryBase<Domain.Entities.Cat, Guid> catRepository, IRepositoryBase<Domain.Entities.Branch, Guid> branchRepository, IEFUnitOfWork efUnitOfWork)
+        public CreateCatCommandHandler(IRepositoryBase<Domain.Entities.Cat, Guid> catRepository, IRepositoryBase<Domain.Entities.Branch, Guid> branchRepository, IEFUnitOfWork efUnitOfWork, IMediaService mediaService)
         {
             _catRepository = catRepository;
             _branchRepository = branchRepository;
             _efUnitOfWork = efUnitOfWork;
+            _mediaService = mediaService;
         }
 
         public async Task<Result> Handle(Command.CreateCatCommand request, CancellationToken cancellationToken)
         {
             var branchFound = await _branchRepository.FindByIdAsync(request.BranchId);
             if (branchFound == null)
-            {
                 throw new BranchException.BranchNotFoundException(request.BranchId);
-            }
+
+            //_mediaService.UploadImage()
+
             var catCreated = Domain.Entities.Cat.CreateCat(request.Sex, request.Name, request.Age, request.Breed, request.Size, request.Color, request.Description, request.BranchId, DateTime.Now, DateTime.Now, false);
             _catRepository.Add(catCreated);
             await _efUnitOfWork.SaveChangesAsync(cancellationToken);
---
+++ src/PawFund.Domain/Entities/ImageCat.cs
@@ -0,0 +1,11 @@
+﻿using PawFund.Domain.Abstractions.Entities;
+
+namespace PawFund.Domain.Entities;
+
+public class ImageCat : DomainEntity<Guid>
+{
+    public string ImageUrl { get; set; }
+
+    public Guid CatId { get; set; }
+    public virtual Cat Cat { get; set; }
+}
