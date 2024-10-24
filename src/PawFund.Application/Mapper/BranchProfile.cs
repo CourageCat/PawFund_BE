a79bfe2f7ef2ad1c83bf916de36f10f77b94502c
CourageCat <minhhien270103@gmail.com>
2024-10-24 15:54:04 +0700

    Get All Branches by Admin

---
+++ src/PawFund.Application/Mapper/BranchProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using PawFund.Contract.Abstractions.Shared;
+using PawFund.Domain.Entities;
+using static PawFund.Contract.Services.Branchs.Response;
+
+namespace PawFund.Application.Mapper;
+
+public class BranchProfile : Profile
+{
+    public BranchProfile()
+    {
+        CreateMap<Branch, BranchResponse>();
+        CreateMap<PagedResult<Branch>, PagedResult<BranchResponse>>();
+    }
+}
---
+++ src/PawFund.Application/UseCases/V1/Queries/Branch/GetAllBranchesQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using PawFund.Contract.Abstractions.Message;
+using PawFund.Contract.Abstractions.Shared;
+using PawFund.Contract.Enumarations.MessagesList;
+using PawFund.Contract.Services.Branchs;
+using PawFund.Contract.Shared;
+using PawFund.Domain.Abstractions.Dappers;
+using PawFund.Domain.Exceptions;
+
+namespace PawFund.Application.UseCases.V1.Queries.Branch;
+
+public sealed class GetAllBranchesQueryHandler : IQueryHandler<Query.GetAllBranchesQuery, Success<PagedResult<Response.BranchResponse>>>
+{
+    private readonly IDPUnitOfWork _dpUnitOfWork;
+    private readonly IMapper _mapper;
+
+    public GetAllBranchesQueryHandler(IDPUnitOfWork dpUnitOfWork, IMapper mapper)
+    {
+        _dpUnitOfWork = dpUnitOfWork;
+        _mapper = mapper;
+    }
+
+    public async Task<Result<Success<PagedResult<Response.BranchResponse>>>> Handle(Query.GetAllBranchesQuery request, CancellationToken cancellationToken)
+    {
+        var listBranches = await _dpUnitOfWork.BranchRepositories.GetPagedAsync(request.PageIndex, request.PageSize, request.FilterParams, request.SelectedColumns);
+        if(listBranches.Items.Count == 0)
+        {
+            throw new BranchException.BranchEmptyException();
+        }
+        var result = _mapper.Map<PagedResult<Response.BranchResponse>>(listBranches);
+        return Result.Success(new Success<PagedResult<Response.BranchResponse>>(MessagesList.BranchGetAllBranchesSuccess.GetMessage().Code, MessagesList.BranchGetAllBranchesSuccess.GetMessage().Message, result));
+
+    }
+}
