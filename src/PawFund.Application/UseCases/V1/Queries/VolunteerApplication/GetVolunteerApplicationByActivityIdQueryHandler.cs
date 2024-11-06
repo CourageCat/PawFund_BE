0ef58631afaab9eefcf8aa24830e6ba80384da6d
nhatquang1605 <honhatquang1605@gmail.com>
2024-11-06 22:34:42 +0700

    Using mapper for GetVolunteerApplicationByActivityId

--- src/PawFund.Application/UseCases/V1/Queries/VolunteerApplication/GetVolunteerApplicationByActivityIdQueryHandler.cs
+++ src/PawFund.Application/UseCases/V1/Queries/VolunteerApplication/GetVolunteerApplicationByActivityIdQueryHandler.cs
@@ -30,26 +30,28 @@ namespace PawFund.Application.UseCases.V1.Queries.VolunteerApplication
 
             if (result != null && result.Items != null && result.Items.Count > 0)
             {
-                var volunteerApplicationDto = result.Items
-                    .Where(item => item != null) // Ensure item is not null
-                    .Select(item => new VolunteerApplicationsDTO
-                    {
-                        Id = item.Id,
-                        Status = item.Status,
-                        Description = item.Description,
-                        ReasonReject = item.ReasonReject,
-                        EventId = item.EventId,
-                        EventActivityId = item.EventActivityId,
-                        Account = item.Account != null ? new AccountDTO // Ensure item.Account is not null
-                        {
-                            Id = item.Account.Id,
-                            FirstName = item.Account.FirstName,
-                            LastName = item.Account.LastName,
-                            Email = item.Account.Email,
-                            PhoneNumber = item.Account.PhoneNumber
-                        } : null // Set to null if item.Account is null
-                    })
-                    .ToList();
+                //var volunteerApplicationDto = result.Items
+                //    .Where(item => item != null) // Ensure item is not null
+                //    .Select(item => new VolunteerApplicationsDTO
+                //    {
+                //        Id = item.Id,
+                //        Status = item.Status,
+                //        Description = item.Description,
+                //        ReasonReject = item.ReasonReject,
+                //        EventId = item.EventId,
+                //        EventActivityId = item.EventActivityId,
+                //        Account = item.Account != null ? new AccountDTO // Ensure item.Account is not null
+                //        {
+                //            Id = item.Account.Id,
+                //            FirstName = item.Account.FirstName,
+                //            LastName = item.Account.LastName,
+                //            Email = item.Account.Email,
+                //            PhoneNumber = item.Account.PhoneNumber
+                //        } : null // Set to null if item.Account is null
+                //    })
+                //    .ToList();
+
+                var volunteerApplicationDto = _mapper.Map<List<VolunteerApplicationsDTO>>(result.Items);
 
                 return Result.Success(new Success<PagedResult<VolunteerApplicationsDTO>>(
                     MessagesList.GetVolunteerApplicationSuccess.GetMessage().Code,
