89b6757c57ef50d1d7d6fa287e938020a9c8314d
nhatquang1605 <honhatquang1605@gmail.com>
2024-11-08 00:17:01 +0700

    Background Job Service

---
+++ src/PawFund.Infrastructure/Worker/HangfireBackgroundJobService.cs
@@ -0,0 +1,20 @@
+﻿using Hangfire;
+using PawFund.Contract.Abstractions.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PawFund.Infrastructure.Worker
+{
+    class HangfireBackgroundJobService : IBackgroundJobService
+    {
+        public void ScheduleRecurringJob(string jobId, Expression<Func<Task>> methodCall, string cronExpression)
+        {
+            // Sử dụng Hangfire để lên lịch job định kỳ
+            RecurringJob.AddOrUpdate(jobId, methodCall, cronExpression);
+        }
+    }
+}
